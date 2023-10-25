c42c8adab2e26ab7cb42c5de31a91e5b97bfb7a0
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2023-10-25 14:47:52 -0700

    Added Docker and Serilog

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ## Note
 
 [EOD Historical Data](https://eodhd.com/)
+
+## Docker
+
+- Port
+	- Config Type: Port
+	- Name: Http
+	- Container Port: 8080
+	- Connection Type: TCP
+- Path
+	- Config Type: Path
+	- Name: App Data
+	- Container Path: /app/data
+	- Access Mode: Read/Write
--- z019.Web.Site/Program.cs
+++ z019.Web.Site/Program.cs
@@ -1,35 +1,92 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
+using Serilog.Events;
+using Serilog;
 using z019.Storage.SqlStorage;
 using z019.Web.Site.Components;
 
-var connectionString = new SqliteConnectionStringBuilder()
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Application Initializing");
+
+try
 {
-    DataSource = @"c:\Code\DB\z019.sqlite",
-    Mode = SqliteOpenMode.ReadWriteCreate,
-}.ToString();
+    var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddMudServices();
-builder.Services.AddPooledDbContextFactory<StorageDbContext>(options => options.UseSqlite(connectionString));
-builder.Services.AddDbContext<StorageDbContext>(options => options.UseSqlite(connectionString));
+    var connectionString = SqliteConnectionString(builder.Configuration);
 
-var app = builder.Build();
+    // Add services to the container.
+    builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+    builder.Services.AddMudServices();
+    builder.Services.AddPooledDbContextFactory<StorageDbContext>(options => options.UseSqlite(connectionString));
+    builder.Services.AddDbContext<StorageDbContext>(options => options.UseSqlite(connectionString));
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+    Log.Information("Application Starting");
+
+    var app = builder.Build();
+
+    Log.Information("Application Started");
+
+    DatabaseMigrate(app.Services);
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    // app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+    app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+    app.Run();
+}
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Application Ended\n");
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-app.Run();
+static string SqliteConnectionString(ConfigurationManager configuration)
+{
+    var path = configuration["SqlitePath"];
+    if (string.IsNullOrEmpty(path)) throw new NullReferenceException(nameof(path));
+
+    var idx = path.LastIndexOf('/');
+    if (idx > 2)
+    {
+        var leftPath = path[..idx];
+        Directory.CreateDirectory(leftPath);
+    }
+
+    return new SqliteConnectionStringBuilder()
+    {
+        DataSource = path,
+        Mode = SqliteOpenMode.ReadWriteCreate,
+    }.ToString();
+}
+
+static void DatabaseMigrate(IServiceProvider services)
+{
+    using var scope = services.CreateScope();
+    using var context = scope.ServiceProvider.GetService<StorageDbContext>();
+    context?.Database.EnsureCreated();
+}
